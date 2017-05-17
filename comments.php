b3affd986f200be2e02327565e038a953fd08eab
Esteban Monge <estebanmonge@riseup.net>
2017-05-16 18:58:17 -0600

    fixing

--- comments.php
+++ comments.php
@@ -1,12 +1,12 @@
 <?php
-    include 'header.php';
+    require 'header.php';
     $ip = $_GET['ip'];
     $vlan = $_GET['vlan'];
     $type = $_GET['type'];
     $pdo = Database::connect();
 ?>
 <body>
-	<?php
+    <?php
            echo drawHeader();
     ?>
     <div class="container-fluid">
@@ -15,32 +15,34 @@
             </div>
             <div class="row">
                 <h3><?php echo $type?> comment for <?php 
+                if ($ip) {
+                    echo 'host '.$ip;
+                    $sql = "SELECT comments FROM hostnames WHERE ip LIKE '".$ip."'";
+                } else {
+                    echo 'vlan '.$vlan;
+                    $sql = "SELECT description AS comments FROM vlans WHERE vlan LIKE '".$vlan."'";
+                }
+        ?>
+    
+    </h3>
+    <form class="form-inline" role="form" action="insert_comment.php" method="POST">
+     <div class="form-group">
+            <TEXTAREA NAME="comment" COLS=40 ROWS=6>
+        <?php
+        foreach ($pdo->query($sql) as $row) {
+            echo $row['comments'];
+        }
+            ?></TEXTAREA>
+       <input type="hidden" name="ip" value="<?php echo $ip?>">
+       <input type="hidden" name="vlan" value="<?php echo $vlan?>">
+     </div>
+        <?php
         if ($ip) {
-            echo 'host '.$ip;
-            $sql = "SELECT comments FROM hostnames WHERE ip LIKE '".$ip."'";
-        } else {
-            echo 'vlan '.$vlan;
-            $sql = "SELECT description AS comments FROM vlans WHERE vlan LIKE '".$vlan."'";
+            echo '<label for="reserved"><input type="checkbox" value="reserved" name="reserved">Reserved?</label>';
         }
         ?>
-		
-		</h3>
-		<form class="form-inline" role="form" action="insert_comment.php" method="POST">
- 		<div class="form-group">
-            <TEXTAREA NAME="comment" COLS=40 ROWS=6><?php
-            foreach ($pdo->query($sql) as $row) {
-                echo $row['comments'];
-            }
-            ?></TEXTAREA>
- 			<input type="hidden" name="ip" value="<?php echo $ip?>">
- 			<input type="hidden" name="vlan" value="<?php echo $vlan?>">
- 		</div>
-		<?php if ($ip) {
-                echo '<label for="reserved"><input type="checkbox" value="reserved" name="reserved">Reserved?</label>';
-            }
-        ?>
-  		<button type="submit" class="btn btn-default">Submit</button>
-		</form>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
             </div> <!-- /row -->
     </div> <!-- /container -->
   </body>
